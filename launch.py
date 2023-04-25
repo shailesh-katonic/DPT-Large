59848ebf3e8f1db8470bd85ebaa3496bd4d20b2a
shailesh-katonic <103512924+shailesh-katonic@users.noreply.github.com>
2023-04-25 14:08:27 +0530

    chore: Add whisper model with load, predict method

---
+++ launch.py
@@ -0,0 +1,22 @@
+import torch
+from transformers import pipeline
+
+
+def loadmodel(logger):
+    """Get model from cloud object storage."""
+   
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    logger.info(f"device: {device}")
+    model = pipeline(model="openai/whisper-large", device=device)
+    return model  
+
+def preprocessing(features, logger):
+    """ Applies preprocessing techniques to the raw data"""
+    logger.info("no preprocessing required")
+    return False
+    
+def predict(features, model, logger):
+    """Predicts the results for the given inputs"""
+    logger.info("model prediction")
+    prediction = model(features)["text"]
+    return prediction
